9e5e9e44d820f4937bfd05d6fcdecf56d16f1462
Enyi Asonye <enyiasonye@gmail.com>
2020-05-17 19:52:03 -0500

    created availability card

--- src/reserve/components/ReservableAreaCard/ReservableAreaCard.jsx
+++ src/reserve/components/ReservableAreaCard/ReservableAreaCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Card, Chip } from '@material-ui/core';
 import styled from 'styled-components';
+import colors from '../../../common/constants/colors';
 
 const CardContent = styled.div`
     padding: 0;
     display: flex;
     overflow: visible;
     flex-direction: column;
+    height: 100%;
     font-family: Muli;
 
     .card-content-header {
-        /* flex: 1 0 auto; */
+        flex: 1 0 auto;
         padding: 1rem;
         padding-bottom: 0rem;
         padding-top: 0.5rem;
@@ -27,41 +29,54 @@ const CardContent = styled.div`
         justify-content: space-between;
         align-items: center;
         flex-shrink: 0;
-        padding: 0rem 1rem;
+        padding: 1rem;
         background: #f9f6f6;
     }
 `
 
+const BestSpot = styled(Chip)`
 
+    background-color: ${colors.primary} !important;
+    color: ${colors.white} !important;
+    border-radius: 0.3rem !important;
+    padding: 0rem 0.75rem !important;
+    position: absolute;
+    right: 2.75rem;
+    top: 0.25rem;  
+`;
+
+// Use a ternary to make best spot show up based on props passed in
 export default (props) => {
 
     return (
-
-        <CardContent>
-            <Card elevation={5}>
-                <div className="card-content-header">
-                    <div>Available from</div>
-                    <div className="availability-time">
-                        {props.timesAvailable.startTime}
-                        {' '}
+        <>
+            <BestSpot color={colors.primary} size="large" label="BEST SPOT" />
+            <Card elevation={5} style={{ padding: 0 }}>
+                <CardContent>
+                    <div className="card-content-header">
+                        <div>Available from</div>
+                        <div className="availability-time">
+                            {props.timesAvailable.startTime}
+                            {' '}
                         -
                         {' '}
-                        {props.timesAvailable.endTime}
+                            {props.timesAvailable.endTime}
+                        </div>
                     </div>
-                </div>
-                <div className="card-content-footer">
-                    <span>
-                        {props.maxDurationMinutes}
-                        {' '}
+                    <div className="card-content-footer">
+                        <span>
+                            {props.maxDurationMinutes}
+                            {' '}
                         min. max
                     </span>
-                    <span>
-                        {props.spaces}
-                        {' '}
+                        <span>
+                            {props.spaces}
+                            {' '}
                         spots free
                     </span>
-                </div>
+                    </div>
+                </CardContent>
             </Card>
-        </CardContent>
+        </>
     );
 };
--- src/reserve/components/ReservationSearch/ReservationSearchPortal.jsx
+++ src/reserve/components/ReservationSearch/ReservationSearchPortal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from '@material-ui/core';
+import { Card, Button } from '@material-ui/core';
 
 import {
   Map,
